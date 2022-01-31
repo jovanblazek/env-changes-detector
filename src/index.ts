7c40f5e208a1af31e4c37d46512a07129605aead
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2022-01-31 15:24:33 +0100

    Simplify markdown message generation

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,61 @@
-import * as core from "@actions/core";
-import { promisify } from "util";
-import { exec } from "child_process";
+import * as core from '@actions/core'
+import { promisify } from 'util'
+import { exec } from 'child_process'
 
 const HAS_DETECTED_CHANGES = 'env-changes-detected'
 const RAW_OUTPUT = 'env-changes-raw'
 const MD_OUTPUT = 'env-changes-md'
 
 async function run() {
-	try {
-		const targetBranch = core.getInput("target-branch");
-		core.setOutput(HAS_DETECTED_CHANGES, false);
-		core.setOutput(RAW_OUTPUT, []);
-		core.setOutput(MD_OUTPUT, "No env file changes detected.");
+  try {
+    const targetBranch = core.getInput('target-branch')
+    core.setOutput(HAS_DETECTED_CHANGES, false)
+    core.setOutput(RAW_OUTPUT, [])
+    core.setOutput(MD_OUTPUT, 'No env file changes detected.')
 
-		const diffResult = await promisify(exec)(
-			`git diff -w origin/${targetBranch} -- '**.env.example' '**.env-test-example'`
-		);
-		if (diffResult.stderr) {
-			throw new Error(diffResult.stderr);
-		}
+    const diffResult = await promisify(exec)(
+      `git diff -w origin/${targetBranch} -- '**.env.example' '**.env-test-example'`
+    )
+    if (diffResult.stderr) {
+      throw new Error(diffResult.stderr)
+    }
 
-		if (diffResult.stdout === "") {
-			console.log('Did not find any changes');
-			return;
-		}
+    if (diffResult.stdout === '') {
+      return
+    }
 
-		const regex =
-			/^(?<diff>[\+-]{1}\w.*)|(?:diff --git\sa(?<file>.*?)\s.*)$/gm;
-		const matches = Array.from(
-			diffResult.stdout.matchAll(regex),
-			(match) => match.groups && (match.groups.file || match.groups.diff)
-		).filter((match) => match !== undefined) as string[];
+    const regex = /^(?<diff>[\+-]{1}\w.*)|(?:diff --git\sa(?<file>.*?)\s.*)$/gm
+    const matches = Array.from(
+      diffResult.stdout.matchAll(regex),
+      (match) => match.groups as { [key: string]: string }
+    )
 
-		// format found changes to markdown syntax
-		const result = matches
-			.map((match, index) => {
-				// file name
-				if (match[0] === "/") {
-					if (index === 0) {
-						return `\`${match}\`\n`;
-					}
-					return `\`\`\`\n\`${match}\`\n`;
-				}
+    // format found changes to markdown syntax
+    const markdownMessage = matches.map((match, index) => {
+      if (match.file) {
+        if (index === 0) {
+          return `\`${match.file}\`\n`
+        }
+        return `\`\`\`\n\`${match.file}\`\n`
+      }
+      const previousMatch = matches[index - 1]
+      if (previousMatch.diff) {
+        return `${match.diff}\n`
+      }
+      return `\`\`\` diff\n${match.diff}\n`
+    })
+    markdownMessage.push('```') // close last code block
 
-				// diff
-				const previousMatch = matches[index - 1];
-				if (previousMatch[0] === "+" || previousMatch[0] === "-") {
-					return `${match}\n`;
-				}
-				return `\`\`\` diff\n${match}\n`;
-			})
-			.join("\n");
-
-		console.log(result);
-		
-		core.setOutput(HAS_DETECTED_CHANGES, true);
-		core.setOutput(RAW_OUTPUT, matches);
-		core.setOutput(
-			MD_OUTPUT,
-			`## Detected changes in env files:\n\n${result}`
-		);
-	} catch (error: any) {
-		console.log(error);
-		core.setFailed(error);
-	}
+    core.setOutput(HAS_DETECTED_CHANGES, true)
+    core.setOutput(RAW_OUTPUT, matches)
+    core.setOutput(
+      MD_OUTPUT,
+      `## Detected changes in env files:\n\n${markdownMessage.join('\n')}`
+    )
+  } catch (error: any) {
+    console.log(error)
+    core.setFailed(error)
+  }
 }
 
-run();
+run()
